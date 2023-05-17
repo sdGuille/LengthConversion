4f1758b8a84a82a5d41f0e6e755d92334ec7d9d2
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-17 10:29:39 -0600

    Figuring out the aplication

--- LengthConversion/ContentView.swift
+++ LengthConversion/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var convertFrom = 0.0
+    @FocusState private var valueIsFocused: Bool
+    var unitsInput = ["km", "mi", "ft", "yd", "m"]
+    var unitsOutput = ["km", "mi", "ft", "yd", "m"]
+    @State private var selectedUnit = "km"
+    @State private var selectOuputUnit = "mi"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Value", value: $convertFrom, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($valueIsFocused)
+                    Picker("unit", selection: $selectedUnit) {
+                        ForEach(unitsInput, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Convert from")
+                }
+                
+                Section {
+                    Picker("unit", selection: $selectOuputUnit) {
+                        ForEach(unitsOutput, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("To")
+                }
+            }
+            .navigationTitle("Convertions")
         }
-        .padding()
     }
 }
 
