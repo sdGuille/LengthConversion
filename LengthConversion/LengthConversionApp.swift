6c9d4eb2f1ddad046fc10e3a859b1d881dd0095f
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-17 06:03:23 -0600

    Initial Commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by MacBook Pro on 17/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
